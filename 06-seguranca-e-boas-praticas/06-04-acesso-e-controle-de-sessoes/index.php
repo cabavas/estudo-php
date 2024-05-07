77b82b36ca1ff0b700d95eed809b03dc7b247e5c
cabavas <cabavas@gmail.com>
2024-05-07 13:55:33 -0300

    F145 - Acesso e controle de sessões

--- 06-seguranca-e-boas-praticas/06-04-acesso-e-controle-de-sessoes/index.php
+++ 06-seguranca-e-boas-praticas/06-04-acesso-e-controle-de-sessoes/index.php
@@ -8,3 +8,22 @@ require __DIR__ . "/../source/autoload.php";
  * [ session ] Uma classe statless para manipulação de sessões
  */
 fullStackPHPClassSession("session", __LINE__);
+
+$session = new \source\Core\Session();
+$session->set("user", 1);
+
+$session->set("stats", ["name", "email"]);
+$session->unset("stats");
+
+if(!$session->has("login")) {
+    echo "<p>Logar-se</p>";
+    $user = (new \source\Models\UserModel())->load(1);
+    $session->set("login", $user->data());
+}
+
+$session->destroy();
+
+var_dump(
+    $_SESSION,
+    $session->all(),
+);
